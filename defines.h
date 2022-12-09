651bc75aa04e02ef6ea5f2371d620e22c9842639
Alex Kiselev <a.kiselev@volz-servos.com>
2022-12-09 16:28:23 +0100

    defines.h added to repository

---
+++ defines.h
@@ -0,0 +1,20 @@
+#ifndef __DEFINES_H
+#define __DEFINES_H
+
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define FLASH_MAX_LEN     0x20000
+
+#endif
